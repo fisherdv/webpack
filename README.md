1b42d02ca31883c1a40955bcf9184d6300009630
Dmitriy <dmitriyfiser@gmail.com>
2021-08-26 21:55:26 +0500

    добавлен babel

---
+++ README.md
@@ -0,0 +1,20 @@
+# Сборка UI с помощью webpack
+
+Установлен ```node.js```
+
+![Npm version](/screenshots/npm_version.png)
+
+Установлен ```babel``` и ```axios```
+
+![Package list](/screenshots/package_list.png)
+
+Запущен ```babel``` 
+
+```sh
+npx babel src -d lib
+or
+npm run babel
+```
+Получен страшный файлик 
+
+![Babel](/screenshots/babel.png)
---
+++ src/index.js
@@ -0,0 +1,26 @@
+class User{
+    constructor(){
+        this.name = "Аноним";
+        this.hello = this.hello.bind(this);
+        this.set_name = this.set_name.bind(this);
+    }
+    hello(){        
+        let el = document.getElementById("name");
+        el.innerText = `Hello ${this.name}`;
+    }
+    set_name(name){
+        this.name = name;
+    }
+}
+
+const user = new User();
+user.hello()
+
+document.getElementById("save").onclick = function() {    
+    let new_name = document.getElementById("input").value;
+    user.set_name(new_name)
+    user.hello();
+};
+
+
+
